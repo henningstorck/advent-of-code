51eef40e0c3b7399bd855e01a582f667d9db2d91
Henning Storck <mail@henningstorck.com>
2022-12-02 08:43:25 +0100

    Solve day 2

---
+++ 2022/day02/day02.go
@@ -0,0 +1,63 @@
+package day02
+
+import (
+	"github.com/henningstorck/advent-of-code/common/functional"
+	"github.com/henningstorck/advent-of-code/common/inputreader"
+)
+
+const (
+	rock1    = "A"
+	paper1   = "B"
+	scissor1 = "C"
+
+	rock2    = "X"
+	paper2   = "Y"
+	scissor2 = "Z"
+
+	lose = "X"
+	draw = "Y"
+	win  = "Z"
+)
+
+var lookupTable1 = map[string]int{
+	rock1 + " " + rock2:       1 + 3,
+	rock1 + " " + paper2:      2 + 6,
+	rock1 + " " + scissor2:    3 + 0,
+	paper1 + " " + rock2:      1 + 0,
+	paper1 + " " + paper2:     2 + 3,
+	paper1 + " " + scissor2:   3 + 6,
+	scissor1 + " " + rock2:    1 + 6,
+	scissor1 + " " + paper2:   2 + 0,
+	scissor1 + " " + scissor2: 3 + 3,
+}
+
+var lookupTable2 = map[string]int{
+	rock1 + " " + lose:    3 + 0,
+	rock1 + " " + draw:    1 + 3,
+	rock1 + " " + win:     2 + 6,
+	paper1 + " " + lose:   1 + 0,
+	paper1 + " " + draw:   2 + 3,
+	paper1 + " " + win:    3 + 6,
+	scissor1 + " " + lose: 2 + 0,
+	scissor1 + " " + draw: 3 + 3,
+	scissor1 + " " + win:  1 + 6,
+}
+
+func Solve(reader *inputreader.InputReader) (int, int) {
+	rounds := reader.ReadLines(2022, 2)
+	return SolvePart1(rounds), SolvePart2(rounds)
+}
+
+func SolvePart1(rounds []string) int {
+	return getPoints(rounds, lookupTable1)
+}
+
+func SolvePart2(rounds []string) int {
+	return getPoints(rounds, lookupTable2)
+}
+
+func getPoints(rounds []string, lookupTable map[string]int) int {
+	return functional.Reduce(rounds, func(points int, round string) int {
+		return points + lookupTable[round]
+	}, 0)
+}
---
+++ 2022/day02/day02_test.go
@@ -0,0 +1,29 @@
+package day02_test
+
+import (
+	"testing"
+
+	"github.com/henningstorck/advent-of-code/2022/day02"
+	"github.com/henningstorck/advent-of-code/common/inputreader"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSolveParts(t *testing.T) {
+	input := []string{
+		"A Y",
+		"B X",
+		"C Z",
+	}
+
+	resultPart1 := day02.SolvePart1(input)
+	resultPart2 := day02.SolvePart2(input)
+	assert.Equal(t, 15, resultPart1)
+	assert.Equal(t, 12, resultPart2)
+}
+
+func TestSolveRealInput(t *testing.T) {
+	reader := inputreader.NewInputReader("../..")
+	resultPart1, resultPart2 := day02.Solve(reader)
+	assert.Equal(t, 14375, resultPart1)
+	assert.Equal(t, 10274, resultPart2)
+}
